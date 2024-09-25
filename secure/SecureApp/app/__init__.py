dbb90da2f5e9db28a40d76a2243055cd83ffc027
GiorMich <madonnakaralh@gmail.com>
2024-09-25 21:54:32 +0300

    Update __init__.py

--- secure/SecureApp/app/__init__.py
+++ secure/SecureApp/app/__init__.py
@@ -8,8 +8,8 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'your-secret-key')
 
 
 # Supabase configuration
-SUPABASE_URL = os.getenv('SUPABASE_URL', 'https://vnpwrbzydaohhowhqgpa.supabase.co')
-SUPABASE_KEY = os.getenv('SUPABASE_KEY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZucHdyYnp5ZGFvaGhvd2hxZ3BhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYyMTQ4MDUsImV4cCI6MjA0MTc5MDgwNX0.HCyEYtAlOYEbkIfkAe6M3gnLWieyxwgvjG5nU5gs4pk')
+SUPABASE_URL = os.getenv('SUPABASE_URL', 'https://***.supabase.co')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY', 'your-supabase-key')
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
